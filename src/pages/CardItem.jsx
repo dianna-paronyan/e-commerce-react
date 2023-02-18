cb5cdc00d670fc8330d6ff31e7706eb17e72a8e1
dianna1234 <01234dianna@gmail.com>
2023-02-19 00:16:34 +0400

    add styles

--- src/pages/CardItem.jsx
+++ src/pages/CardItem.jsx
@@ -1,27 +1,30 @@
-import { useCartItems } from "../Provider/CartProvider"
+import { useCartItems } from "../Provider/CartProvider";
+import "./CartItem.css";
 
 function CardItem() {
-  const {cart,setCart} = useCartItems();
+  const { cart, setCart } = useCartItems();
 
-  function removeCartItem(id){
-    setCart(cart.filter((el)=>el.id !== id))
+  function removeCartItem(id) {
+    setCart(cart.filter((el) => el.id !== id));
   }
 
   return (
-    <div>
-      CardItem
-      {cart?.map((el)=>{
-        return(
-          <div key={el.id}>
-            <img src={el.image} alt="" />
-            <h4>{el.name}</h4>
-            <p>{el.price}</p>
-            <button onClick={()=>removeCartItem(el.id)}>Delete</button>
-          </div>
-        )
-      })}
-    </div>
-  )
+    <>
+      <h1>Cart Items</h1>
+      <div className="cart_box">
+        {cart?.map((el) => {
+          return (
+            <div key={el.id} className="cards_of_cart">
+              <img src={el.image} alt="" />
+              <h4>{el.name}</h4>
+              <p>{el.price}</p>
+              <button onClick={() => removeCartItem(el.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export default CardItem
+export default CardItem;
