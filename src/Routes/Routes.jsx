76a5ab847e9356238ee6ec0a138f5c3b70011b3f
dianna1234 <01234dianna@gmail.com>
2023-02-18 22:54:39 +0400

    add cartItem route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,17 +3,22 @@ import Home from "../pages/Home";
 import Products from "../pages/Products";
 import Layout from "../components/Layout";
 import Product from "../pages/Product";
+import CartProvider from "../Provider/CartProvider";
+import CardItem from "../pages/CardItem";
 
 function MyRoutes() {
   return (
     <div>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/:id" element={<Product />} />
-        </Route>
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/:id" element={<Product />} />
+            <Route path="/cartItems" element={<CardItem />} />
+          </Route>
+        </Routes>
+      </CartProvider>
     </div>
   );
 }
