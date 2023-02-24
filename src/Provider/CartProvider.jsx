341e7ddfd6f6f4d3dade446e9f5bbb93ddf40f59
dianna1234 <01234dianna@gmail.com>
2023-02-24 14:09:56 +0400

    delete console log

--- src/Provider/CartProvider.jsx
+++ src/Provider/CartProvider.jsx
@@ -14,17 +14,14 @@ export default function CartProvider({ children }) {
   function addToCart(product, id) {
     const existItem = cart.find((pr) => pr.id === id);
     const someEl = cart.some((pr) => pr.id === id);
-    console.log(someEl, "some");
-    console.log(product, "product");
+
     if (!someEl) {
       setCart([...cart, product]);
-      console.log(cart, "if");
     } else {
       const newItem = cart.map((el) =>
         el.id === id ? { ...existItem, quantity: existItem.quantity + 1 } : el
       );
       setCart(newItem);
-      console.log(cart, "else");
     }
   }
 
