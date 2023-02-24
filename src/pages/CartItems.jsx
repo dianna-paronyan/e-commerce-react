e3dc8a506a65849c1ed58e04cad40b3b6675d93f
dianna1234 <01234dianna@gmail.com>
2023-02-24 20:33:22 +0400

    show table if cart length more than 0

--- src/pages/CartItems.jsx
+++ src/pages/CartItems.jsx
@@ -14,49 +14,52 @@ function CartItems() {
     <div className="content">
       <h1>Shopping Cart</h1>
       <div className="cart_box">
-        <table>
-          <thead>
-            <tr>
-              <th>Description</th>
-              <th>Quantity</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.map((el) => {
-              return (
-                <tr className="row" key={el.id}>
-                  <td className="desc">
-                    <img src={el.image} alt="" />
-                    <div className="text_btn_box">
-                      <h5>{el.name}</h5>
-                      <p>{el.description}</p>
-                      <p
-                        className="delete_icon"
-                        onClick={() => removeCartItem(el.id)}
-                      >
-                        <DeleteIcon
-                          sx={{ fontSize: "15px", color: "#3e3e3e" }}
-                        />
-                        Remove
-                      </p>
-                    </div>
-                  </td>
-                  <td>
-                    <Counter
-                      num={el.quantity}
-                      id={el.id}
-                      setCart={setCart}
-                      cart={cart}
-                      removeCartItem={removeCartItem}
-                    />
-                  </td>
-                  <td>{el.price} AMD</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        {cart.length > 0 ? (
+                  <table>
+                  <thead>
+                    <tr>
+                      <th>Description</th>
+                      <th>Quantity</th>
+                      <th>Price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cart.map((el) => {
+                      return (
+                        <tr className="row" key={el.id}>
+                          <td className="desc">
+                            <img src={el.image} alt="" />
+                            <div className="text_btn_box">
+                              <h5>{el.name}</h5>
+                              <p>{el.description}</p>
+                              <p
+                                className="delete_icon"
+                                onClick={() => removeCartItem(el.id)}
+                              >
+                                <DeleteIcon
+                                  sx={{ fontSize: "15px", color: "#3e3e3e" }}
+                                />
+                                Remove
+                              </p>
+                            </div>
+                          </td>
+                          <td>
+                            <Counter
+                              num={el.quantity}
+                              id={el.id}
+                              setCart={setCart}
+                              cart={cart}
+                              removeCartItem={removeCartItem}
+                            />
+                          </td>
+                          <td>{el.price} AMD</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+        ) : <></>}
+
         <h3 className="amount">
           Estimated Total is  {cart.reduce((acc, el) => acc + el.price * el.quantity, 0)} AMD
         </h3>
